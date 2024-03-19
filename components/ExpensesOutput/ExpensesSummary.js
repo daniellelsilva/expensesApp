6f950590df8ae8c217d3de97db2b93f0d7f4e64c
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-19 19:11:34 -0300

    add expenses structure

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function ExpensesSummary() {
+  return (
+    <View>
+      <Text>Last 7 days</Text>
+      <Text>$177.95</Text>
+    </View>
+  )
+}
