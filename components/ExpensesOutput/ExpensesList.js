076ef8aa8f551fc0e4ac5371df82d2d2ed37bd6d
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-19 19:30:36 -0300

    add key extractor

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -14,6 +14,7 @@ export default function ExpensesList({expenses}) {
     <FlatList
       data={expenses}
       renderItem={renderExpensesItem}
+      keyExtractor={(item) => item.id}
     />
   )
 }
