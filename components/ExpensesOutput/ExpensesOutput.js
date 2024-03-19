cfbb9cf3550732591dbcb6ec039c4046a5af77e0
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-19 19:17:48 -0300

    add expenses summary logic

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,10 +3,10 @@ import { View } from 'react-native'
 import ExpensesSummary from './ExpensesSummary'
 import ExpensesList from './ExpensesList'
 
-export default function ExpensesOutput({expenses}) {
+export default function ExpensesOutput({expenses, period}) {
   return (
     <View>
-      <ExpensesSummary />
+      <ExpensesSummary expenses={expenses} period={period} />
       <ExpensesList />
     </View>
   )
