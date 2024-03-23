3548dcf548ca074c93add9e77e7ee70ad563f79d
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-23 18:05:46 -0300

    add style

--- components/ExpensesOutput/ExpensesSummary.js
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
+import { GlobalStyles } from '../../constants/styles';
 
 export default function ExpensesSummary({ expenses, period }) {
   const expensesSum = expenses.reduce((sum, expense) => {
@@ -7,9 +8,29 @@ export default function ExpensesSummary({ expenses, period }) {
   }, 0 );
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>{period}</Text>
       <Text>${expensesSum.toFixed(2)}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 8,
+    backgroundColor: GlobalStyles.colors.primary50,
+    borderRadius: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  period: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary400
+  },
+  sum: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: GlobalStyles.colors.primary500
+  }
+})
