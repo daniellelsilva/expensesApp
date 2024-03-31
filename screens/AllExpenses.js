123a296682df9bd7c8405e274083437b94fd49d4
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-31 19:14:50 -0300

    use expense

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { ExpensesContext } from '../store/expenses-context'
 
 export default function AllExpenses() {
+  const expensesCtx = useContext(ExpensesContext)
   return (
     <View style={styles.container}>
-      <ExpensesOutput expensesPeriod="Total" />
+      <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" />
     </View>
   )
 }
