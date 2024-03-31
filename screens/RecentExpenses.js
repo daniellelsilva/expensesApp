2c97419700af60e7fca633dbc7f3746497df78db
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-31 19:16:53 -0300

    use context

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { StyleSheet, View } from 'react-native'
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput'
+import { ExpensesContext } from '../store/expenses-context'
 
 export default function RecentExpenses() {
+  const expensesCtx = useContext(ExpensesContext);
+
+  const recentExpenses = expensesCtx.expenses.filter((expense) => {
+    const today = new Date();
+    const date7DaysAgo = getDateMinusDays(today, 7);
+
+    return (expense.date >= date7DaysAgo) && (expense.date <= today);
+  });
+
   return (
     <View style={styles.container}>
-      <ExpensesOutput expensesPeriod="Last 7 days" />
+      <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 days" />
     </View>
   )
 }
