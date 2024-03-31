c2e330cecead927eaca91b36aea1581e31b7b962
daniellelsilva <danielle.luisasilva@gmail.com>
2024-03-31 12:35:38 -0300

    add context functions

--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,3 +1,36 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-export const ExpensesContext = createContext();
+export const ExpensesContext = createContext({
+  expenses: [],
+  addExpense: ({ description, amount, date }) => {},
+  deleteExpense: (id) => {},
+  updateExpense: (id, { description, amount, date }) => {},
+});
+
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case 'ADD':
+    case 'UPDATE':
+    case 'DELETE':
+    default:
+      return state
+  }
+}
+
+export default function ExpensesContextProvider({ children }) {
+  const [expepensesState, dispatch] = useReducer(expensesReducer);
+
+  function addExpense(expenseData) {
+    dispatch({ type: 'ADD', payload: expenseData})
+  }
+
+  function deleteExpense(id) {
+    dispatch({ type: 'DELETE', payload: id})
+  }
+
+  function updateExpense(id, expenseData) {
+    dispatch({ type: 'UPDATE', payload: {id: id, data:expenseData}})
+  }
+
+  return <ExpensesContext.Provider>{ children }</ExpensesContext.Provider>
+}
