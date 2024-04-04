8e7dda7c030f5d413738aa27d331834e68ca1a5f
daniellelsilva <danielle.luisasilva@gmail.com>
2024-04-04 19:22:53 -0300

    add inputs

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,12 @@
 import { TextInput, View } from "react-native";
+import Input from "./Input";
 
 export default function ExpenseForm() {
   return (
     <View>
-      
+      <Input label="Amount" />
+      <Input label="Date" />      
+      <Input label="Description" />      
     </View>
   )
 }
